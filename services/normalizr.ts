b10e66e56ab23343f29cece1cf03f8c443b8f7f6
vitoraraujodev <vitoraraujo.dev@gmail.com>
2021-01-14 22:14:22 -0300

    Add Actions and Reducers

--- services/normalizr.ts
+++ services/normalizr.ts
@@ -2,10 +2,17 @@ import { normalize, schema } from "normalizr";
 
 function normalizeData(data: [object]): object {
   // Defines plans schema
-  const planSchema = new schema.Entity("plans");
+  const plan = new schema.Entity("plans");
+
+  const user = new schema.Entity("users", {
+    plan: plan
+  });
 
   // Defines storytellers schema
-  const storytellers = new schema.Entity("storytellers", { plan: planSchema });
+  const storytellers = new schema.Entity("storytellers", {
+    user: user,
+    plan: plan
+  });
 
   const normalizedData = normalize(data, storytellers);
 
