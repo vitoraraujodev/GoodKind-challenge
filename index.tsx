b42e01ff9f405c161d050d69bb6c5f6009776eaf
vitoraraujodev <vitoraraujo.dev@gmail.com>
2021-01-15 20:16:59 -0300

    Trying to solve compiling forever

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 // import { axios } from "axios";
